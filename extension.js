f7a5a07124819dca097f8a04654734de8a71a6a6
TropicalShadow <ddlogger123@gmail.com>
2024-03-21 21:04:26 +0000

    refactor: clean comments, and format code

--- extension.js
+++ extension.js
@@ -1,87 +1,98 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 const fs = require("fs");
-const snippets = require('./snippets/snippets.json');
+const snippets = require("./snippets/snippets.json");
 const TemplateCogCode = require("./snippets/TemplateCog.json");
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log('Pythond DiscordAPI VSCode Extension" is now active!');
 
-	console.log('Pythond DiscordAPI VSCode Extension" is now active!');
+  var createBot = vscode.commands.registerCommand(
+    "pythondiscordapivscodeextension.createbot",
+    function () {
+      vscode.window.showInformationMessage("Creating Bot Files Now...");
+      if (vscode.workspace.workspaceFolders == undefined) {
+        vscode.window.showWarningMessage("You do not have a workspace open!");
+        return;
+      }
 
-/* 	var testcmd = vscode.commands.registerCommand("pythondiscordapivscodeextension.test",function (){
-		var main = snippets["Main Bot File"].body.join('\n');
-		console.log(main);
-	}); */
-	var createBot = vscode.commands.registerCommand("pythondiscordapivscodeextension.createbot", function () {
-		vscode.window.showInformationMessage('Creating Bot Files Now...');
-		if(vscode.workspace.workspaceFolders == undefined){
-			vscode.window.showWarningMessage("You do not have a workspace open!")
-			return;
-		}
-		const folder = vscode.workspace.workspaceFolders[0].uri.fsPath
-		vscode.window.showInputBox({prompt:"Bot Name: ",placeHolder:"(DemonicBot)"}).then(BotName =>{
-			if(!BotName){
-				vscode.window.showErrorMessage("Failed to give a valid input.");
-				return
-			}
-			fs.stat(folder+`/${BotName}`, function(err, stat) {
-				if(err == null) {
-					if(stat.isDirectory()){
-						fs.readdir(folder+`/${BotName}`,function (err,files) {
-							if(files.length == 0){
-								vscode.window.showInformationMessage(`Found folder called ${BotName}. attempting to add files.`)
-							}else{
-								vscode.window.showErrorMessage(`${BotName} is already a folder with content. Remove content to continue.`);
-								return;
-							}
-						});
-					}
-				} else if(err.code == 'ENOENT') {
-					vscode.window.showInformationMessage(`Creating Folder Named ${BotName}`)
-					fs.mkdir(folder+`/${BotName}`, function (err){
-						if(err != null){
-							vscode.window.showErrorMessage(`Error Creating Folder named ${BotName} - ${err.code}`);
-							return;
-						}
-					});
-				} else {
-					//console.log('Error While Checking Folder: ', err.code);
-					vscode.window.showErrorMessage(`Error Generated - ${err.code}`);
-					return;
-				}
-				const subfolder = folder+`/${BotName}`;//Bot Folder
-				const main = snippets["Main Bot File"].body.join('\n');//MAIN CODE
-				//console.log(main);
-				fs.writeFile(subfolder+`/main.py`,main,()=>{});//Main File
-				fs.writeFile(subfolder+`/TOKEN.key`,"REPLACE THIS WITH API TOKEN",()=>{});//Token File
-				fs.mkdirSync(subfolder+`/cogs`);//COGS FOLDER
-				const cogfolder = subfolder+`/cogs`;//COGS FOLDER LOCATION
-				//var Templatecog = snippets["Cog Template file"].body.join('\n');//Cog Template Code
-				fs.writeFile(cogfolder+`/Template.py`,TemplateCogCode["main cog"].join("\n"),()=>{});//COG TEMPLATE FILE
-				console.log(`${BotName} Created `);//FINISHED OUTPUT
-				vscode.window.showInformationMessage(`${BotName} has been created.`)
-			});
+      const folder = vscode.workspace.workspaceFolders[0].uri.fsPath;
+      vscode.window
+        .showInputBox({ prompt: "Bot Name: ", placeHolder: "(DemonicBot)" })
+        .then((BotName) => {
+          if (!BotName) {
+            vscode.window.showErrorMessage("Failed to give a valid input.");
+            return;
+          }
+          fs.stat(folder + `/${BotName}`, function (err, stat) {
+            if (err == null) {
+              if (stat.isDirectory()) {
+                fs.readdir(folder + `/${BotName}`, function (err, files) {
+                  if (files.length == 0) {
+                    vscode.window.showInformationMessage(
+                      `Found folder called ${BotName}. attempting to add files.`,
+                    );
+                  } else {
+                    vscode.window.showErrorMessage(
+                      `${BotName} is already a folder with content. Remove content to continue.`,
+                    );
+                    return;
+                  }
+                });
+              }
+            } else if (err.code == "ENOENT") {
+              vscode.window.showInformationMessage(
+                `Creating Folder Named ${BotName}`,
+              );
+              fs.mkdir(folder + `/${BotName}`, function (err) {
+                if (err != null) {
+                  vscode.window.showErrorMessage(
+                    `Error Creating Folder named ${BotName} - ${err.code}`,
+                  );
+                  return;
+                }
+              });
+            } else {
+              //console.log('Error While Checking Folder: ', err.code);
+              vscode.window.showErrorMessage(`Error Generated - ${err.code}`);
+              return;
+            }
+            const subfolder = folder + `/${BotName}`; //Bot Folder
+            const main = snippets["Main Bot File"].body.join("\n"); //MAIN CODE
+            //console.log(main);
+            fs.writeFile(subfolder + `/main.py`, main, () => {}); //Main File
+            fs.writeFile(
+              subfolder + `/TOKEN.key`,
+              "REPLACE THIS WITH API TOKEN",
+              () => {},
+            ); //Token File
+            fs.mkdirSync(subfolder + `/cogs`); //COGS FOLDER
+            const cogfolder = subfolder + `/cogs`; //COGS FOLDER LOCATION
+            //var Templatecog = snippets["Cog Template file"].body.join('\n');//Cog Template Code
+            fs.writeFile(
+              cogfolder + `/Template.py`,
+              TemplateCogCode["main cog"].join("\n"),
+              () => {},
+            ); //COG TEMPLATE FILE
+            console.log(`${BotName} Created `); //FINISHED OUTPUT
+            vscode.window.showInformationMessage(
+              `${BotName} has been created.`,
+            );
+          });
+        });
+    },
+  );
 
-
-
-		})
-
-		
-	});
-
-	context.subscriptions.push(createBot);
-	//context.subscriptions.push(testcmd);
+  context.subscriptions.push(createBot);
 }
 // this method is called when your extension is deactivated
 function deactivate() {
-	vscode.window.showInformationMessage(`Thank you for using my extension!`)
+  vscode.window.showInformationMessage(`Thank you for using my extension!`);
 }
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
